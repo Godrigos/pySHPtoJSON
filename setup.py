f7106e0450ca110a323fc14b51dce2d1ef8eaac2
Rodrigo Aluizio <r.aluizio@gmail.com>
2021-11-27 16:41:58 -0300

    Updated setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,20 @@
 from cx_Freeze import setup, Executable
+import os
+import sys
 
 executables = [Executable('pySHPtoJSON.py')]
+
+BUILD_EXE_OPTIONS = {
+    'build_exe': r'bin',
+    'excludes': ['matplotlib', 'sqlite3', 'scipy', 'tkinter'],
+    'path': [os.path.abspath('pySHPtoJSON')] + sys.path,
+}
 
 setup(
     name='pySHPtoGeoJ',
     version='1.0',
     description='A CLI tool to download and convert shapefiles of Brazilian'
     ' territory made available by IBGE.',
+    options={'build_exe': BUILD_EXE_OPTIONS},
     executables=executables,
 )
